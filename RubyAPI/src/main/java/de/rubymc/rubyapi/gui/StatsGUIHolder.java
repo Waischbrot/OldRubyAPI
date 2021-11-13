c33b5ff522748d8fa7395ce95d214ad23eeb353c
Waischbrot <92989284+Waischbrot@users.noreply.github.com>
2021-11-13 17:30:26 +0100

    added statsgui

---
+++ RubyAPI/src/main/java/de/rubymc/rubyapi/gui/StatsGUIHolder.java
@@ -0,0 +1,10 @@
+package de.rubymc.rubyapi.gui;
+
+import org.bukkit.Bukkit;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+
+public class StatsGUIHolder implements InventoryHolder {
+
+    public Inventory statsGUI = Bukkit.createInventory(this, 45)
+}
